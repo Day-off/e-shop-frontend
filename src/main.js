617734fd46b3a4c12ce5e4be498a506a76a27728
jevolo <jevolo@ttu.ee>
2022-10-31 20:25:15 +0200

    Create Registration field. Through this registration field user is able to register and all the data is saves into database.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueAxios from "vue-axios"
+import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App).use(router)
+app.use(VueAxios, axios)
+app.provide("axios", app.config.globalProperties.axios)
+app.mount("#app")

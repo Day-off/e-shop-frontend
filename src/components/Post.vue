62b70dbbfde47d4ea46e1c3beaa7de2d709810c3
damana <damana@ttu.ee>
2022-11-12 10:39:53 +0200

    add post button added

---
+++ src/components/Post.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class = "container">
+    <h1 class="text-center"> Posts List </h1>
+    <table class="table table-striped">
+      <caption></caption>
+      <thead>
+      <th> Post Id </th>
+      <th> Post Head </th>
+      <th> Post Description </th>
+      <th> Post Image </th>
+      </thead>
+      <tbody>
+      <tr v-for = "post in posts" v-bind:key = "post.id">
+        <td> {{post.postId}}</td>
+        <td> {{post.head}}</td>
+        <td> {{post.description}}</td>
+        <td> {{post.imageLink}}</td>
+
+      </tr>
+      </tbody>
+
+
+    </table>
+  </div>
+</template>
+
+<script>
+import PostService from "@/router/PostService";
+export default {
+
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Posts',
+  data(){
+    return {
+      posts : []
+    }
+
+  },
+  methods: {
+    getPosts(){
+      PostService.getPosts().then((response) =>{
+        this.posts = response.data;
+      });
+    }
+  },
+  created() {
+    this.getPosts()
+
+  }
+
+
+}
+</script>
---
+++ src/views/CreatePostView.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="row">
+    <div class="col-sm-3 mx-auto">
+      <h1>Add Post</h1>
+      <br>
+      <form class="row g-3" @submit="postData">
+        <div class="col-md-5">
+          <label for="inputHead" class="form-label">Head</label>
+          <input type="text" class="form-control" id="inputHead" v-model="posts.head">
+        </div>
+        <div class="col-md-7">
+          <label for="inputImageLink" class="form-label">Image</label>
+          <input type="file" class="form-control" id="inputImageLink">
+        </div>
+        <div class="col-12">
+          <label for="inputDescription" class="form-label">Description</label>
+          <input type="text" class="form-control" id="inputDescription" v-model="posts.description">
+        </div>
+        <div class="col-12">
+          <button type="submit" class="btn btn-primary">Create Post</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data(){
+    return {
+      posts: {
+        head: null,
+        description: null,
+        imageLink: null,
+      }
+    }
+  },
+  methods: {
+    postData() {
+      this.posts = axios.post("/api/posts", this.posts)
+    }
+  }
+};
+</script>
+
+<style>
+label {
+  margin-top: 15px;
+}
+</style>

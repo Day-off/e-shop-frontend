bf5d2584f64e592a0e4fd9994578b0e0044b1860
jevolo <jevolo@ttu.ee>
2023-01-12 21:43:34 +0200

    UserProfile combined with CreatePost view.

--- src/views/CreatePostView.vue
+++ src/views/CreatePostView.vue
@@ -43,9 +43,7 @@ export default {
       },
       formData: null,
       uploaded: false,
-      emailUser: "",
-      nameUser: "",
-      secondNameUser: ""
+      emailUser: ""
     }
   },
   methods: {

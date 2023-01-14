6e5d12c0004beb372677ae157b5717b4b4d3467a
jevolo <jevolo@ttu.ee>
2023-01-14 14:21:16 +0200

    UserProfile combined with CreatePost view.

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,13 @@ function logout() {
 </template>
 
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
 nav {
   padding: 30px;

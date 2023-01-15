c65605cf00bbf43c2519219173cbaae83f633ad6
jevolo <jevolo@ttu.ee>
2023-01-15 13:35:25 +0200

    AboutPage

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,94 +1,54 @@
 <template>
-  <div id="users">
-    <div class="col-sm-3 mx-auto">
-      <figure>
-        <figcaption>Users</figcaption>
-        <table>
-          <tr>
-            <th>ID</th>
-            <th>First name</th>
-            <th>Last name</th>
-          </tr>
-          <tr v-for="user of users" :key="user.id">
-            <td>{{ user.id }}</td>
-            <td>{{ user.firstName }}</td>
-            <td>{{ user.lastName }}</td>
-          </tr>
-        </table>
-      </figure>
+  <div class="about-page">
+    <div class="about-page__header">
+      <h1 class="about-page__title" style="color: white;">About Us</h1>
     </div>
-  </div>
-
-  <div id="products">
-    <div class="col-sm-3 mx-auto">
-      <figure>
-        <figcaption>Products</figcaption>
-        <table>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Description</th>
-          </tr>
-          <tr v-for="product of products" :key="product.head">
-            <td>{{ product.head }}</td>
-            <td>{{ product.description }}</td>
-          </tr>
-        </table>
-      </figure>
+    <div class="about-page__content">
+      <p class="about-page__description">Bulletin board - fresh ads of individuals about the sale and purchase of goods of all categories. The easiest way to sell or buy things.</p>
+      <div class="about-page__image-container">
+        <img class="about-page__image" src="../assets/lolotype.png" alt="About Us">
+      </div>
     </div>
   </div>
 </template>
 
-<style>
-#users { position: absolute; left: 0; top: 10%; width: 70%; }
-#products { position: absolute; right: 0; top: 10%; width: 70%; }
-
-figcaption {
-  font-size: 35px
-}
-
-table {
-  border-collapse: separate;
-  border-spacing: 0 20px;
-  font-size: 20px;
-  word-break: break-word;
-}
-th {
-  border-bottom: 1px solid #3a3a3a;
-}
-td {
-  width: 150px;
+<style scoped>
+.about-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   text-align: center;
-  padding: 5px;
-  border-bottom: 1px solid #ababab;
 }
-tr {
 
+.about-page__header {
+  background-color: #46b8da;
+  padding: 1rem;
 }
-tr:hover {
-  background-color: #D6EEEE;
+
+.about-page__title {
+  color: #31B0D5;
+  font-size: 2rem;
+}
+
+.about-page__content {
+  padding: 2rem;
+}
+
+.about-page__description {
+  color: #666;
+  font-size: 1.2rem;
+  line-height: 1.5;
+  text-align: left;
+  margin-bottom: 2rem;
+}
+
+.about-page__image-container {
+  width: 100%;
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+.about-page__image {
+  width: 50%;
 }
 </style>
-
-<script>
-import axios from "axios";
-
-export default {
-  data(){
-    return {
-      users: [],
-      posts: []
-    }
-  },
-  async created() {
-    let token = JSON.parse(localStorage.getItem("token"));
-    let response = await axios.get('/api/public/posts');
-    this.posts = response.data;
-    if (token != null) {
-      axios.defaults.headers.common["Authorization"] = "Bearer " + token
-      response = await axios.get('/api/users');
-      this.users = response.data;
-    }
-  }
-}
-</script>
